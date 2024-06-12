f537efcdb2fa310bc5af8c6b15000cea73cbc1a4
Nirbhay Lourembam <32927418+nirbhay-codes@users.noreply.github.com>
2024-06-12 22:31:56 +0530

    create get users api route

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -6,6 +6,19 @@ export const test = (req, res) => {
   res.json({ message: 'API is working!' })
 }
 
+export const deleteUser = async (req, res, next) => {
+  // Sample: req.user { id: '6666e4391a044a3e15bc7135', iat: 1718022856 }
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, 'You are not allowed to delete this user'))
+  }
+  try {
+    await User.findByIdAndDelete(req.params.userId)
+    res.status(200).json('User has been deleted')
+  } catch (error) {
+    next(error)
+  }
+}
+
 // /api/user/update/:userId
 export const updateUser = async (req, res, next) => {
   // Validate user authorization
@@ -24,19 +37,6 @@ export const updateUser = async (req, res, next) => {
     const updatedUser = await updateUserInDatabase(req.params.userId, req.body)
     const { password, ...rest } = updatedUser._doc
     res.status(200).json(rest)
-  } catch (error) {
-    next(error)
-  }
-}
-
-export const deleteUser = async (req, res, next) => {
-  // Sample: req.user { id: '6666e4391a044a3e15bc7135', iat: 1718022856 }
-  if (req.user.id !== req.params.userId) {
-    return next(errorHandler(403, 'You are not allowed to delete this user'))
-  }
-  try {
-    await User.findByIdAndDelete(req.params.userId)
-    res.status(200).json('User has been deleted')
   } catch (error) {
     next(error)
   }
@@ -103,6 +103,50 @@ function validateUserInput(body) {
 export const signout = (req, res, next) => {
   try {
     res.clearCookie('access_token').status(200).json('User has been signed out')
+  } catch (error) {
+    next(error)
+  }
+}
+
+// GET http://localhost:5173/api/user/getusers?startIndex=0&limit=9&sort=asc
+export const getUsers = async (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, 'You are not allowed to see all users'))
+  }
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0
+    const limit = parseInt(req.query.limit) || 9
+    const sortDirection = req.query.sort === 'asc' ? 1 : -1
+
+    const users = await User.find()
+      .sort({ createdAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit)
+
+    const usersWithoutPassword = users.map((user) => {
+      const { password, ...rest } = user._doc
+      return rest
+    })
+
+    const totalUsers = await User.countDocuments()
+
+    const now = new Date()
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    )
+
+    const lastMonthUsers = await User.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    })
+
+    res.status(200).json({
+      users: usersWithoutPassword,
+      totalUsers,
+      lastMonthUsers,
+    })
   } catch (error) {
     next(error)
   }
